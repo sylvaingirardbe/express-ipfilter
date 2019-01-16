f4102cdb11a506408aa8c0430bc2fa01730854cb
Sylvain Girard <sylvain@ecm-technologies.eu>
2019-01-16 21:31:30 +0100

    Updated all packages, removed dependency on publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,7 @@ module.exports = function(grunt){
     eslint: {
       all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
+        fix: true,
         'ecmaVersion': 6
       }
     },
